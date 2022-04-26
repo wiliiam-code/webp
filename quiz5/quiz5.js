cd0f38fe7fd88f69b91fdb46fc5b44675865642d
wiliiam-code <66834192+wiliiam-code@users.noreply.github.com>
2022-04-26 23:23:21 +0800

    Update quiz5.js

--- quiz5/quiz5.js
+++ quiz5/quiz5.js
@@ -1,7 +1,7 @@
 
 var textNode = document.createTextNode("hello cgu!");
 $(document).ready(function(){
-    txt = "<li>hello cgu!</li>";
+    txt = "<li>Nice click<3</li>";
     $("#myList").click(function(){
         $("#myList").append(txt);
         console.log($("ul li:last").text());
